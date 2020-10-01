c8a62dcd8fa1564005469c89f6c49da04fc25c52
Estefania Pichardo <171G0242@rcarbonifera.tecnm.mx>
2020-10-01 14:22:58 -0500

    ServidorPeliculas

---
+++ ServidorPeliculas/Pelicula.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorPeliculas
+{
+    public enum Sala { A1 = 0, A2 = 1, B1 = 2, B2 = 3 };
+    public enum Clasificacion { A = 0, B15 = 1, C = 2, R = 3 };
+    public enum Idioma { Español = 0, Subtitulada = 1 };
+    public class Pelicula : INotifyPropertyChanged
+    {
+        private string hora;
+        private string nombre;
+        private Sala sala;
+        private Clasificacion clasificacion;
+        private Idioma idioma;
+
+        public string Hora
+        {
+            get => hora;
+            set
+            {
+                hora = value;
+                LanzarEvento("Hora");
+            }
+        }
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                nombre = value;
+                LanzarEvento("Nombre");
+            }
+        }
+        public Sala Sala
+        {
+            get => sala; set
+            {
+                sala = value;
+                LanzarEvento("Sala");
+            }
+        }
+        public Clasificacion Clasificacion
+        {
+            get => clasificacion;
+            set
+            {
+                clasificacion = value;
+                LanzarEvento("Clasificacion");
+            }
+        }
+        public Idioma Idioma
+        {
+            get => idioma;
+            set
+            {
+                idioma = value;
+                LanzarEvento("Idioma");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void LanzarEvento(string propiedad)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("propiedad"));
+        }
+    }
+}
---
+++ ServidorPeliculas/PeliculasServer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+
+namespace ServidorPeliculas
+{
+    public class PeliculasServer
+    {
+        public CatalogoPeliculas CatalogoPeliculas { get; set; } = new CatalogoPeliculas();
+
+        HttpListener listener;
+
+        Dispatcher dispatcher;
+
+        public PeliculasServer()
+        {
+            listener = new HttpListener();
+            dispatcher = Dispatcher.CurrentDispatcher;
+
+            listener.Prefixes.Add("http://*:8080/Actividad5/");
+            listener.Start();
+            listener.BeginGetContext(OnRequest, null);
+
+        }
+
+        private void OnRequest(IAsyncResult ar)
+        {
+            var context = listener.EndGetContext(ar);
+            listener.BeginGetContext(OnRequest, null);
+
+            if (context.Request.Url.LocalPath == "/Actividad5/Tablero")
+            {
+                if (context.Request.HttpMethod == "GET")
+                {
+                    var datos = JsonConvert.SerializeObject(CatalogoPeliculas.ListaPeliculas);
+                    byte[] buffer = Encoding.UTF8.GetBytes(datos);
+
+                    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                    context.Response.StatusCode = 200;
+                }
+                else
+                {
+                    if (context.Request.ContentType.StartsWith("application/json") & context.Request.ContentLength64 > 0)
+                    {
+                        StreamReader reader = new StreamReader(context.Request.InputStream);
+                        var datos = reader.ReadToEnd();
+                        var pelicula = JsonConvert.DeserializeObject<Pelicula>(datos);
+
+                        dispatcher.Invoke(new Action(() =>
+                        {
+                            if (context.Request.HttpMethod == "POST")
+                            {
+                                CatalogoPeliculas.Agregar(pelicula);
+                            }
+                            else if (context.Request.HttpMethod == "PUT")
+                            {
+                                CatalogoPeliculas.Editar(pelicula);
+                            }
+                            else if(context.Request.HttpMethod=="DELETE")
+                            {
+                                CatalogoPeliculas.Eliminar(pelicula);
+                            }
+
+                        }));
+                        context.Response.StatusCode = 200;
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+            context.Response.Close();
+
+        }
+    }
+}
